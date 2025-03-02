165a8dcc0b51e071bde22491258de4dca2a5fa44
Carlos <carlos97181034535@gmail.com>
2025-03-02 23:36:21 +0100

    scripts for the landing page moved to this folder

---
+++ src/js/landing/functions.js
@@ -0,0 +1,26 @@
+import { Notification } from '../classes/Notification.js';
+import { emailObj, form, inputEmail } from './constants.js';
+
+export function valueToEmailObj(event) {
+  emailObj.email = event.target.value;
+}
+
+export function submitForm(event) {
+  event.preventDefault();
+  const { email } = emailObj;
+
+  if (!validateEmail(email)) {
+    new Notification('Please, enter a valid email address', 'error');
+    return;
+  }
+
+  new Notification('Thank you for subscribing', 'success');
+  form.reset();
+  inputEmail.value = '';
+  emailObj.email = '';
+}
+
+function validateEmail(email) {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
