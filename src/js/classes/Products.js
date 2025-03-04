7477471f9e74b4150952da5527fe951e167f7bc3
Carlos <carlos97181034535@gmail.com>
2025-03-04 18:14:59 +0100

    class to manage everything related to the products

--- src/js/classes/Products.js
+++ src/js/classes/Products.js
@@ -4,6 +4,25 @@ export class Products {
   }
 
   renderProducts(products) {
-    console.log(products)
+    products.forEach(product => {
+      const divProductsContainer = document.querySelector('#div-products-container');
+      const { id, title, price, category: { name }, images: [image] } = product;
+
+      const divProduct = document.createElement('div');
+      divProduct.classList.add('product-card');
+      divProduct.innerHTML = `
+        <a href="product.html">
+          <img loading="lazy" src="${image}" class="image-product" alt="Product Image">
+        </a>
+        <h3 class="product-title">${title}</h3>
+        <p class="product-category-name">${name}</p>
+        <div class="div-product-buy">
+          <p class="price">Price: <span class="price-span">$${price}</span></p>
+          <button class="btn-product-add-to-cart">Add To Cart</button>
+        </div>
+      `;
+
+      divProductsContainer.appendChild(divProduct)
+    })
   }
 }
