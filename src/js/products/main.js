19f112f24121bb00db5df14c8b477acf655e5068
Carlos <carlos97181034535@gmail.com>
2025-03-04 18:16:14 +0100

    minor change

--- src/js/products/main.js
+++ src/js/products/main.js
@@ -1,9 +1,7 @@
 import { getProducts } from "./functions.js";
 import { formProducts } from "./constants.js";
 
-window.addEventListener('load', () => {
-  formProducts.reset();
-});
+window.addEventListener('load', () => formProducts.reset());
 
 document.addEventListener('DOMContentLoaded', () => {
   getProducts()
