3960f6e55f26be97bd5830e604ef32a8ef7e5ff9
Carlos <carlos97181034535@gmail.com>
2025-03-05 19:30:48 +0100

    spinner deleted when get the products

--- src/js/classes/Products.js
+++ src/js/classes/Products.js
@@ -4,6 +4,9 @@ export class Products {
   }
 
   renderProducts(products) {
+    const removeSpinner = document.querySelector('#div-spinner');
+    removeSpinner.classList.add('hide-spinner');
+
     products.forEach(product => {
       const divProductsContainer = document.querySelector('#div-products-container');
       const { id, title, price, category: { name }, images: [image] } = product;
@@ -11,8 +14,8 @@ export class Products {
       const divProduct = document.createElement('div');
       divProduct.classList.add('product-card');
       divProduct.innerHTML = `
-        <a href="product.html">
-          <img loading="lazy" src="${image}" class="image-product" alt="Product Image">
+        <a href="product.html?id=${id}">
+          <img loading="lazy" src="${image}" class="product-image" alt="Product Image">
         </a>
         <h3 class="product-title">${title}</h3>
         <p class="product-category-name">${name}</p>
