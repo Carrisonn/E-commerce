c3012878dd9bd0df0e95f3b843c9b9f2467af9e3
Carlos <carlos97181034535@gmail.com>
2025-03-06 23:32:37 +0100

    events for the form and their respective imports have been added

--- src/js/products/main.js
+++ src/js/products/main.js
@@ -1,8 +1,10 @@
-import { getProducts } from "./functions.js";
-import { formProducts } from "./constants.js";
+import { getProducts, valueToProductObj, validateData } from "./functions.js";
+import { formProducts, inputSearchProducts } from "./constants.js";
 
 window.addEventListener('load', () => formProducts.reset());
 
 document.addEventListener('DOMContentLoaded', () => {
-  getProducts()
+  getProducts();
+  inputSearchProducts.addEventListener('input', valueToProductObj);
+  formProducts.addEventListener('submit', validateData);
 });
