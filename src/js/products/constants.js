7b3021d78ef519d6c0c55cc69d8857d3583ebc3e
Carlos <carlos97181034535@gmail.com>
2025-03-06 23:43:23 +0100

    select necessary html elements

--- src/js/products/constants.js
+++ src/js/products/constants.js
@@ -1,6 +1,6 @@
-export const formProducts = document.querySelector('#form-products');
 export const divProductsContainer = document.querySelector('#div-products-container');
+export const divProductsForm = document.querySelector('.div-product-form');
+export const inputSearchProducts = document.querySelector('#input-search-products');
+export const formProducts = document.querySelector('#form-products');
 
-//export const userProductObj = {
-//product: ''
-//}
+export const userProductObj = { product: '' };
