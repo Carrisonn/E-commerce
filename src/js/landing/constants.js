d936ff63e8d8c53ad31b84120a946c7038b654a5
Carlos <carlos97181034535@gmail.com>
2025-03-06 23:30:14 +0100

    variable renamed

--- src/js/landing/constants.js
+++ src/js/landing/constants.js
@@ -1,7 +1,5 @@
-export const divForm = document.querySelector('.div-form');
+export const divFormLanding = document.querySelector('.div-form-landing');
 export const form = document.querySelector('#form');
 export const inputEmail = document.querySelector('#email');
 
-export const emailObj = {
-  email: ''
-}
+export const emailObj = { email: '' };
