1565e7f0878311a31a17c65be5585e9589004643
Carlos <carlos97181034535@gmail.com>
2025-03-07 19:53:19 +0100

    minor changes

--- src/js/landing/functions.js
+++ src/js/landing/functions.js
@@ -3,7 +3,7 @@ import { emailObj, form, inputEmail } from './constants.js';
 
 export function valueToEmailObj(event) {
   emailObj.email = event.target.value;
-}
+};
 
 export function submitForm(event) {
   event.preventDefault();
@@ -17,9 +17,9 @@ export function submitForm(event) {
   new Notification('Thank you for subscribing', 'success', 'landingForm');
   emailObj.email = '';
   form.reset();
-}
+};
 
 function validateEmail(email) {
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return regex.test(email);
-}
+};
