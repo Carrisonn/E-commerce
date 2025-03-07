5d29d5610d86a87c0e0ace2a2eb965f1980d6e80
Carlos <carlos97181034535@gmail.com>
2025-03-07 19:52:39 +0100

    a method has been renamed and added a new one

--- src/js/classes/Products.js
+++ src/js/classes/Products.js
@@ -1,12 +1,12 @@
 import { Notification } from './Notification.js';
-import { divProductsContainer } from '../products/constants.js';
+import { divProductsContainer, formProducts, userProductObj } from '../products/constants.js';
 
 export class Products {
   constructor() {
     this.products = [];
   }
 
-  renderProducts(products) {
+  renderAllProducts(products) {
     const removeSpinner = document.querySelector('#div-spinner');
     removeSpinner.classList.add('hide-spinner');
 
@@ -22,7 +22,7 @@ export class Products {
       const divProduct = document.createElement('div');
       divProduct.classList.add('product-card');
       divProduct.innerHTML = `
-        <a href="product.html?id=${id}">
+        <a href="product.html?id=${id}"> 
           <img loading="lazy" src="${image}" class="product-image" alt="Product Image">
         </a>
         <h3 class="product-title">${title}</h3>
@@ -36,5 +36,13 @@ export class Products {
       const divProductsContainer = document.querySelector('#div-products-container');
       divProductsContainer.appendChild(divProduct);
     })
+    userProductObj.product = '';
+    formProducts.reset();
+  };
+
+  renderSingleProduct(product) {
+    const removeSpinner = document.querySelector('#div-spinner');
+    removeSpinner.classList.add('hide-spinner');
+    console.log(product);
   }
 };
