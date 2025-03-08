e7dd8a2508783ac6cf1093797291cb00801c334c
Carlos <carlos97181034535@gmail.com>
2025-03-08 20:10:35 +0100

    the query string to don't have any fetch errors has been validated

--- src/js/product/functions.js
+++ src/js/product/functions.js
@@ -1,9 +1,9 @@
 import { Products } from "../classes/Products.js";
+import { queryParams } from "./constants.js";
 
 const productInstance = new Products();
 
 export function validateQueryParams() {
-  const queryParams = window.location.search;
   queryParams.slice(0, 4) === '?id=' ? getProduct() : window.location.href = '/';
 };
 
@@ -11,18 +11,21 @@ async function getProduct() {
   const divSpinner = document.querySelector('#div-spinner');
   divSpinner.classList.remove('hide-spinner');
 
-  const queryParams = window.location.search;
   const productId = queryParams.slice(4);
-  const productIdToNumber = Number(productId);
+  if (productId === '') return window.location.href = '/';
 
   try {
-    const productUrl = `https://api.escuelajs.co/api/v1/products/${productIdToNumber}`;
+    const productUrl = `https://api.escuelajs.co/api/v1/products/${productId}`;
     const response = await fetch(productUrl);
+    if (!response.ok) {
+      alert(`Producto no encontrado por el siguiente motivo:\n ${response.statusText}.`);
+      return window.location.href = '/';
+    }
     const product = await response.json();
     productInstance.renderSingleProduct(product);
   } catch (error) {
     console.error(error);
-    alert(`Ha ocurrido un error al buscar el producto:\n ${error}.\n Pulse aceptar para volver a intentarlo`);
-    window.location.reload();
+    alert(`Ha ocurrido un error al buscar el producto:\n ${error}.`);
+    window.location.href = '/';
   }
 };
