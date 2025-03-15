3d09ab59c8860f86573f393a871b86a4ee92e829
Carlos <carlos97181034535@gmail.com>
2025-03-16 00:08:30 +0100

    a method to render the products of the cart has been added

--- src/js/classes/Products.js
+++ src/js/classes/Products.js
@@ -1,6 +1,7 @@
 import { Notification } from './Notification.js';
 import { divProductsContainer, formProducts, userProductObj } from '../products/constants.js';
 import { divProductContainer } from '../product/constants.js';
+import { divCartUserProducts } from '../cart/constants.js';
 
 export class Products {
   constructor() {
@@ -48,9 +49,9 @@ export class Products {
     removeSpinner.classList.add('hide-spinner');
 
     const { title, price, description, category: { name }, images: [image1, image2, image3] } = product;
-    const divProductCard = document.createElement('div');
-    divProductCard.classList.add('single-product-card');
-    divProductCard.innerHTML = `
+    const divProduct = document.createElement('div');
+    divProduct.classList.add('single-product-card');
+    divProduct.innerHTML = `
       <div class="div-images-container">
         <div class="div-small-images">
           <img loading="lazy" src="${image2}" class="product-image" title="${title}" alt="${title}">
@@ -68,7 +69,24 @@ export class Products {
         </div>
       </div>
     `;
-    divProductContainer.appendChild(divProductCard);
+    divProductContainer.appendChild(divProduct);
+  }
+
+  renderCartProducts(products) {
+    products.forEach(product => {
+      const { id, title, price, image } = product;
+      const divProduct = document.createElement('div');
+      divProduct.classList.add('product-card');
+      divProduct.innerHTML = `
+        <img src="${image}" class="product-image" title="${title}" alt="${title}">
+        <h3 class="product-title">${title}</h3>
+        <div class="div-product-cart-price-delete">
+          <p class="price">Price: <span class="price-span">${price}</span></p>
+          <button class="btn-delete-product" data-id="${id}"> Delete</button>
+        </div>
+      `;
+      divCartUserProducts.appendChild(divProduct);
+    });
   }
 
   productAddedToCart(product) {
